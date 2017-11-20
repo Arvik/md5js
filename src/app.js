7e9066fd28a3d59e9f1c977ec14b11cde146c4c7
Viktar Arlou <varlou@gmail.com>
2017-11-20 09:07:34 +0100

    file reading draft

--- src/app.js
+++ src/app.js
@@ -13,16 +13,16 @@ function dropHandler(event) {
   log("drop");
   event.preventDefault();
   var dt = event.dataTransfer;
-  alert('1234');
   if (dt.items) {
     let ul = document.createElement("ul");
     for (var i = 0; i < dt.items.length; i++) {
       if (dt.items[i].kind == "file") {
         let li = document.createElement("li");
         var f = dt.items[i].getAsFile();
-        let md5=calculateMd5(f);
-        let text = `File:${f.name} : md5=[${md5}]`;
-        li.appendChild(document.createTextNode(text));
+        let md5=calculateMd5(f, (md5) => {
+          let text = `File:${f.name} : md5=[${md5}]`;
+          li.appendChild(document.createTextNode(text));
+        } );
         ul.appendChild(li);
       }
     }
@@ -34,15 +34,28 @@ function preventDefault(event) {
   event.preventDefault();
 }
 
-function calculateMd5(file) {
+function calculateMd5(file, md5setText) {
   let pos = 0;
   let buf = 4096;
+  let prevReadChunk = null;
+  let firstRead = null;
   while (pos < file.size) {
     let fileReader = new FileReader();
     let size = Math.min(pos + buf, file.size);
     let blob = file.slice(pos, size);
+    let md5txt = 0;
+    // chaining file reads
+    
+    let readFileFn = () => { fileReader.readAsArrayBuffer(blob); };
     pos += size;
-    fileReader.readAsBinaryString(blob);
+    if (prevReadChunk) {
+        prevReadChunk.onload = (e) => {
+           console.log(event); 
+           readFileFn(); 
+          }
+    } else firstRead = readFileFn;
+    prevReadChunk = fileReader;    
   }
-  return pos;
+  firstRead();
+  md5setText('testmd5');
 }
