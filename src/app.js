ca1ff443698a280d7908ff9fc1fdd46bdd9bad56
Viktar Arlou <viktar@backbase.com>
2017-12-26 18:35:45 +0100

    updated UI

--- src/app.js
+++ src/app.js
@@ -19,43 +19,54 @@ function dropHandler(event) {
       if (dt.items[i].kind == "file") {
         let li = document.createElement("li");
         var f = dt.items[i].getAsFile();
-        let md5=calculateMd5(f, (md5) => {
-          let text = `File:${f.name} : md5=[${md5}]`;
-          li.appendChild(document.createTextNode(text));
-        } );
+        calculateMd5(f)
+          .then(md5 => {
+            let text = `File:${f.name} : md5=[${md5}]`;
+            li.appendChild(document.createTextNode(text));
+          })
+          .catch(err => {
+            let text = `File:${f.name} : IO error=[${err}]`;
+            li.appendChild(document.createTextNode(text));
+          });
         ul.appendChild(li);
       }
     }
     window.md5.appendChild(ul);
-  } 
+  }
 }
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
-function calculateMd5(file, md5setText) {
-  let pos = 0;
-  let buf = 4096;
-  let prevReadChunk = null;
-  let firstRead = null;
-  while (pos < file.size) {
-    let fileReader = new FileReader();
-    let size = Math.min(pos + buf, file.size);
-    let blob = file.slice(pos, size);
-    let md5txt = 0;
-    // chaining file reads
-    
-    let readFileFn = () => { fileReader.readAsArrayBuffer(blob); };
-    pos += size;
-    if (prevReadChunk) {
-        prevReadChunk.onload = (e) => {
-           console.log(event); 
-           readFileFn(); 
-          }
-    } else firstRead = readFileFn;
-    prevReadChunk = fileReader;    
-  }
-  firstRead();
-  md5setText('testmd5');
+function calculateMd5(file) {
+  return new Promise((ok, nok) => {
+    let pos = 0;
+    let buf = 4096;
+    let prevReadChunk = null;
+    let firstRead = null;
+    while (pos < file.size) {
+      let fileReader = new FileReader();
+      let size = Math.min(pos + buf, file.size);
+      let blob = file.slice(pos, size);
+      // chaining file reads
+      let readFileFn = () => {
+        fileReader.readAsArrayBuffer(blob);
+      };
+      pos += size;
+      if (prevReadChunk) {
+        prevReadChunk.onload = e => {
+          //update MD5 
+          //TODO: implement MD5 calculation
+
+          // continue reading the file
+          readFileFn();
+        };
+      } else firstRead = readFileFn;
+      prevReadChunk = fileReader;
+      prevReadChunk.onerror = nok;
+    }
+    prevReadChunk.onload = ok("test");
+    firstRead();
+  });
 }
